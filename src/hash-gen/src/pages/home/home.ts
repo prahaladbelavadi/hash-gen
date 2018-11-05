54a7e9322df05e6e613c52de10f331357832a2e0
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-11-06 00:58:05 +0530

    need to dynamically tie input to output

--- src/hash-gen/src/pages/home/home.ts
+++ src/hash-gen/src/pages/home/home.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as sha256 from "fast-sha256";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+digest = ""
+  constructor(public navCtrl: NavController) { }
+  hashInput(input){
+    const hasher = new sha256.Hash();
+    hasher.update(input);
+    const result = hasher.digest();
+    console.log(result);
+    this.digest = result
   }
-
 }
